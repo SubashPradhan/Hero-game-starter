0ef12f3209652e5f327c5ebcc532074b7ef84833
Subash Pradahan <suprowilde5@gmail.com>
2019-07-06 10:35:12 +0200

    first object added

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,11 @@
 // Write your JS here
+const hero = {
+    name: "Subash",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "Knife",
+        damage: 2
+    }
+}
