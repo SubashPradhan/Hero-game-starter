d57fc56785a4fac3b17f35934b1e28a626e52931
Subash Pradahan <suprowilde5@gmail.com>
2019-07-06 19:56:12 +0200

    Cleaning code

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -40,11 +40,11 @@ function resetHealth(event) {
 
 // Pickup Item Function
 function pickUpItem(object2, newWeapon) {
-    return object2.inventory.push(newWeapon) 
+    return object2.inventory.push(newWeapon)
 }
 
 // Second Picture
-let link_2= 'https://images-na.ssl-images-amazon.com/images/I/41DeqznqpYL._SX425_.jpg'
+let link_2 = 'https://images-na.ssl-images-amazon.com/images/I/41DeqznqpYL._SX425_.jpg'
 const img_2 = document.createElement('img')
 img_2.src = link_2;
 img_2.id = 'dagger';
@@ -55,22 +55,21 @@ mochaImg.appendChild(img_2)
 img_2.addEventListener('click', addDagger)
 
 // Adding Dagger through picture
-function addDagger(event){
-    hero.inventory.push({type: 'dagger', damage: 2})
+function addDagger(event) {
+    hero.inventory.push({ type: 'dagger', damage: 2 })
     return true
 }
 
-// Adding function for weapon equipment
-function equipWeapon(object3) {    
-    if(object3.inventory.length === 0){
+// Weapon equipment funtion
+function equipWeapon(object3) {
+    if (object3.inventory.length === 0) {
         return null
     }
     object3.weapon = object3.inventory[0]
     return true
 }
-
 //Third Picture
-let link_3= 'https://img1.cohimg.net/is/image/Coach/58298_liblk_a0?fmt=jpg&wid=680&hei=885&bgc=f0f0f0&fit=vfit&qlt=75'
+let link_3 = 'https://img1.cohimg.net/is/image/Coach/58298_liblk_a0?fmt=jpg&wid=680&hei=885&bgc=f0f0f0&fit=vfit&qlt=75'
 const img_3 = document.createElement('img')
 img_3.src = link_3;
 img_3.id = 'bag';
@@ -78,11 +77,11 @@ img_3.alt = 'ImageBag';
 
 let bag = document.getElementById('bag')
 mochaImg.appendChild(img_3)
- img_3.addEventListener('click', takeWeapon)
+img_3.addEventListener('click', takeWeapon)
 
 
 /// Last function for equipment
- function takeWeapon(event){
+function takeWeapon(event) {
     hero.weapon = hero.inventory[0]
- }
+}
 
